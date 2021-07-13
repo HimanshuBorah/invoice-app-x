a578ba616d375b1266061c7aaab50da34520f358
Himanshu Borah <borahhimanshu3@gmail.com>
2021-07-13 10:37:13 +0530

    APT

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const express = require('express');
 
-const clientRouter = require('./routes/clientRoutes');
-// const invoiceRouter = require('./routes/invoiceRoutes');
-
-const morgan = require('morgan');
-
 const app = express();
 
-// Use Morgan for dev
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
 
-app.use(express.json());
 
 
 // Mounting routers
-app.use('/api/v1/clients', clientRouter);
-// app.use('/api/v1/users', invoiceRouter);
+app.use('/', (req, res) => {
+  res.send('mY APP')
+});
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,31 +1,7 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
-// Set the DB connection string
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
 
-// Connect using DB sting
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then((con) => {
-    const dbName = con.connections[0].name;
-    const dbUser = con.connections[0].user;
-    console.log(`Database connection was successful`);
-    console.log(`dbName: ${dbName}, dbUser: ${dbUser}`);
-  });
-
-
-const port = process.env.PORT || 3000;
+const port = 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port} ...`);
 });
